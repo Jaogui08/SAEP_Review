3366316e7e8c96d7d5a9c9ea8a7714569a3ff8b2
Jaogui08 <salesfabriciojg@gmail.com>
2026-09-22 09:31:57 -0300

    Revisão Encapsulameto

---
+++ produtos-ts/index.ts
@@ -0,0 +1,13 @@
+import readLine from "readline-sync";
+import {Produto} from "./src/models/Produto.js";
+
+const ObjProduto = new Produto("Laranja",4);
+
+console.log(ObjProduto.getNome);
+console.log("R$ " + ObjProduto.getPreco);
+
+ObjProduto.setNome = readLine.question("Digite o novo nome: ");
+ObjProduto.setPreco = readLine.questionFloat("Digite o novo preco: ");
+
+console.log("\n" + ObjProduto.getNome);
+console.log("R$ " + ObjProduto.getPreco);
